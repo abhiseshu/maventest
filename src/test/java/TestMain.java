f40743dd5ae9fa7c733b5cde3793e0928d2e9455
Adurti Devi Abhiseshu <dead20@student.bth.se>
2021-03-05 16:07:59 +0100

    First commit on Maven Project

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMain {
+    Main main;
+
+    @Test
+    public void testAdd(){
+        main = new Main();
+        assertEquals(2,main.add(1,1));
+    }
+}
